b9573735236c992a24b8522007cabbc0024f9726
ding <112>
2018-10-29 20:13:50 +0800

    feat: 添加vaultFilter

--- src/main/java/com/alibaba/druid/filter/config/VaultFilter.java
+++ src/main/java/com/alibaba/druid/filter/config/VaultFilter.java
@@ -21,22 +21,23 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 import com.alibaba.druid.proxy.jdbc.DataSourceProxy;
 import com.alibaba.druid.support.logging.Log;
 import com.alibaba.druid.support.logging.LogFactory;
+import com.alibaba.druid.util.JdbcUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class VaultFilter extends FilterAdapter {
+    private static Log LOG = LogFactory.getLog(VaultFilter.class);
 
-    private static Log         LOG                     = LogFactory.getLog(VaultFilter.class);
+    public static final String VAULT_GATEWAY = "vault.gateway";
+    public static final String VAULT_AUTHTYPE = "vault.authType";
+    public static final String VAULT_TOKEN = "vault.token";
+    public static final String VAULT_DECRYPTPATH = "vault.decryptPath";
 
-    public static final String CONFIG_FILE             = "config.file";
-    public static final String CONFIG_DECRYPT          = "config.decrypt";
-    public static final String CONFIG_KEY              = "config.decrypt.key";
-
-    public static final String SYS_PROP_CONFIG_FILE    = "druid.config.file";
-    public static final String SYS_PROP_CONFIG_DECRYPT = "druid.config.decrypt";
-    public static final String SYS_PROP_CONFIG_KEY     = "druid.config.decrypt.key";
-
-    public VaultFilter(){
+    public VaultFilter() {
     }
 
     public void init(DataSourceProxy dataSourceProxy) {
@@ -44,28 +45,23 @@ public class VaultFilter extends FilterAdapter {
             LOG.error("ConfigLoader only support DruidDataSource");
         }
         DruidDataSource dataSource = (DruidDataSource) dataSourceProxy;
+        Properties connectionProperties = dataSource.getConnectProperties();
 
-        decrypt(dataSource, null);
+        Properties configFileProperties = loadPropertyFromConfigFile(connectionProperties);
+
+        decrypt(dataSource, configFileProperties);
     }
 
     public void decrypt(DruidDataSource dataSource, Properties info) {
 
         try {
-            String encryptedPassword = null;
-            if (info != null) {
-                encryptedPassword = info.getProperty(DruidDataSourceFactory.PROP_PASSWORD);
-            }
+            String encryptedPassword = dataSource.getPassword();
+            String gateway = info.getProperty(VAULT_GATEWAY);
+            String authType = info.getProperty(VAULT_AUTHTYPE);
+            String token = info.getProperty(VAULT_TOKEN);
+            String decryptPath = info.getProperty(VAULT_DECRYPTPATH);
 
-            if (encryptedPassword == null || encryptedPassword.length() == 0) {
-                encryptedPassword = dataSource.getConnectProperties().getProperty(DruidDataSourceFactory.PROP_PASSWORD);
-            }
-
-            if (encryptedPassword == null || encryptedPassword.length() == 0) {
-                encryptedPassword = dataSource.getPassword();
-            }
-
-
-            String passwordPlainText = new VaultTools().decrypt(encryptedPassword);
+            String passwordPlainText = new VaultTools(gateway, authType, decryptPath, token).decrypt(encryptedPassword);
 
             if (info != null) {
                 info.setProperty(DruidDataSourceFactory.PROP_PASSWORD, passwordPlainText);
@@ -75,5 +71,55 @@ public class VaultFilter extends FilterAdapter {
         } catch (Exception e) {
             throw new IllegalArgumentException("Failed to decrypt.", e);
         }
+    }
+
+    Properties loadPropertyFromConfigFile(Properties connectionProperties) {
+        String configFile = "classpath:vault.properties";
+        Properties info = loadConfig(configFile);
+
+        if (info == null) {
+            throw new IllegalArgumentException("Cannot load remote config file from the [config.file=" + configFile
+                    + "].");
+        }
+        return info;
+    }
+
+    public Properties loadConfig(String filePath) {
+        Properties properties = new Properties();
+
+        InputStream inStream = null;
+        try {
+            if (filePath.startsWith("file://")) {
+                filePath = filePath.substring("file://".length());
+                inStream = getFileAsStream(filePath);
+            } else if (filePath.startsWith("classpath:")) {
+                String resourcePath = filePath.substring("classpath:".length());
+                inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
+            } else {
+                inStream = getFileAsStream(filePath);
+            }
+            if (inStream == null) {
+                LOG.error("load vault config file error, file : " + filePath);
+                return null;
+            }
+            properties.load(inStream);
+            return properties;
+        } catch (Exception ex) {
+            LOG.error("load config file error, file : " + filePath, ex);
+            return null;
+        } finally {
+            JdbcUtils.close(inStream);
+        }
+    }
+
+    private InputStream getFileAsStream(String filePath) throws FileNotFoundException {
+        InputStream inStream = null;
+        File file = new File(filePath);
+        if (file.exists()) {
+            inStream = new FileInputStream(file);
+        } else {
+            inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+        }
+        return inStream;
     }
 }
